57c5dc57cd8bd2bb5710903e9c580cc217092357
Guillaume Smaha <guillaume.smaha@gmail.com>
2023-12-03 14:55:25 +0100

    Wrap JSON.parse into userScriptStartAdBlock function

--- src/adblock.js
+++ src/adblock.js
@@ -2,37 +2,39 @@
 /* global fetch:writable */
 import { configRead } from './config';
 
-export function userScriptStartAdBlock() {}
+export function userScriptStartAdBlock() {
 
-/**
- * This is a minimal reimplementation of the following uBlock Origin rule:
- * https://github.com/uBlockOrigin/uAssets/blob/3497eebd440f4871830b9b45af0afc406c6eb593/filters/filters.txt#L116
- *
- * This in turn calls the following snippet:
- * https://github.com/gorhill/uBlock/blob/bfdc81e9e400f7b78b2abc97576c3d7bf3a11a0b/assets/resources/scriptlets.js#L365-L470
- *
- * Seems like for now dropping just the adPlacements is enough for YouTube TV
- */
-const origParse = JSON.parse;
-JSON.parse = function () {
-  const r = origParse.apply(this, arguments);
-  if (r.adPlacements && configRead('enableAdBlock')) {
-    console.log('Adblock Removed !');
-    r.adPlacements = [];
-  }
+  /**
+   * This is a minimal reimplementation of the following uBlock Origin rule:
+   * https://github.com/uBlockOrigin/uAssets/blob/3497eebd440f4871830b9b45af0afc406c6eb593/filters/filters.txt#L116
+   *
+   * This in turn calls the following snippet:
+   * https://github.com/gorhill/uBlock/blob/bfdc81e9e400f7b78b2abc97576c3d7bf3a11a0b/assets/resources/scriptlets.js#L365-L470
+   *
+   * Seems like for now dropping just the adPlacements is enough for YouTube TV
+   */
+  const origParse = JSON.parse;
+  JSON.parse = function () {
+    const r = origParse.apply(this, arguments);
+    if (r.adPlacements && configRead('enableAdBlock')) {
+      console.log('Adblock Removed !');
+      r.adPlacements = [];
+    }
 
-  // Drop "masthead" ad from home screen
-  if (
-    r?.contents?.tvBrowseRenderer?.content?.tvSurfaceContentRenderer?.content
-      ?.sectionListRenderer?.contents &&
-    configRead('enableAdBlock')
-  ) {
-    console.log('Adblock Removed (2) !');
-    r.contents.tvBrowseRenderer.content.tvSurfaceContentRenderer.content.sectionListRenderer.contents =
-      r.contents.tvBrowseRenderer.content.tvSurfaceContentRenderer.content.sectionListRenderer.contents.filter(
-        (elm) => !elm.tvMastheadRenderer
-      );
-  }
+    // Drop "masthead" ad from home screen
+    if (
+      r?.contents?.tvBrowseRenderer?.content?.tvSurfaceContentRenderer?.content
+        ?.sectionListRenderer?.contents &&
+      configRead('enableAdBlock')
+    ) {
+      console.log('Adblock Removed (2) !');
+      r.contents.tvBrowseRenderer.content.tvSurfaceContentRenderer.content.sectionListRenderer.contents =
+        r.contents.tvBrowseRenderer.content.tvSurfaceContentRenderer.content.sectionListRenderer.contents.filter(
+          (elm) => !elm.tvMastheadRenderer
+        );
+    }
 
-  return r;
-};
+    return r;
+  };
+
+}
